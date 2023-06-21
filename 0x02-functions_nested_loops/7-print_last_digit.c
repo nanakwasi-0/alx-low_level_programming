6a9efa483bc14cf4dc0832ffbc4abc6dac62cf63
Nana <nankwasi999@gmail.com>
2023-06-20 20:07:30 -0700

    Last digit task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - This function prints the last digit of a number
+ * @n: The input parameter
+ * Return: 0 (Success!!)
+ */
+int print_last_digit(int n)
+{
+	int m = n % 10;
+	if (m < 0)
+	{
+		_putchar(-l + 48);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + 48);
+		return (l);
+	}
+}
