368149fa9a4d98364dbf0b1862d74a1ed7e1096c
Nana <nankwasi999@gmail.com>
2023-06-20 18:59:12 -0700

    Fixes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,12 +3,10 @@
  * print_alphabet - Function to print alphabets 10 times
  * Return: None(Success!!)
  */
-
-
 void print_alphabet_x10(void)
 {
-        int i;
-        int j;
+	int i;
+	int j;
 
         for (i = 1; i <= 10; i++)
         {
