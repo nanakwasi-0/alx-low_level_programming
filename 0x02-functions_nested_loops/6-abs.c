92c0e9a3439444f073df55b8f458d39f73ef21d6
Nana <nankwasi999@gmail.com>
2023-06-20 19:50:15 -0700

    Absolute value tasks

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - This function computes the absolute value of an int
+ * @n: The value to be converted.
+ * Return: 0(Success!!)
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (i);
+	}
+	else if (n < 0)
+	{
+		return (i * -1);
+	}
+}
+
