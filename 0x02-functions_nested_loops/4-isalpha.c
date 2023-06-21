c9f55d7193429a97ebeea2f68494d6ec374dc735
Nana <nankwasi999@gmail.com>
2023-06-20 19:26:05 -0700

    isalphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - Check whether parameter is alphabet
+ * @c: The input paramter or formal parameter
+ * Return: 1 or 0(Success!!)
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
