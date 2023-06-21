fecf758624ba8219709d90b15a7a749031f97b8b
Nana <nankwasi999@gmail.com>
2023-06-20 19:38:54 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - This function the prints tje sign of a number
+ * @n: This is the input t be checked
+ * Return: 0 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
