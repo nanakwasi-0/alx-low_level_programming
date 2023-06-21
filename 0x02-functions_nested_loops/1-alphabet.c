3829a0cd4af5e6f4905c86ce2f34224df38f0fa8
Nana <nankwasi999@gmail.com>
2023-06-20 18:22:45 -0700

    1-alphabet task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Return: 0(Successful!!)
+ */
+void print_alphabet(void)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar("\n");
+	}
+	return (0);
+}
