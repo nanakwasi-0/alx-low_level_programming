51e4cd2129a5164bbb8bd7382601f5027f43fe4f
Nana <nankwasi999@gmail.com>
2023-06-22 16:33:17 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - A function that checks if an entry is a number
+ * @c: takes a formal integer parameter called c
+ * Return: 1 if digit and 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	if (x >= 48 && x <= 57);
+	{
+		return (1);
+	}
+	return (0);
+}
