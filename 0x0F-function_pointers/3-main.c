c2f6956f88a1c1204e2d9641f78ade384df549a6
Nana <nankwasi999@gmail.com>
2023-07-19 18:17:28 -0700

    Fix 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,59 +1,48 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
 #include "3-calc.h"
+/**
+* main - Prints the result of simple operations.
+* @argc: The number of arguments supplied to the program.
+* @argv: An array of pointers to the arguments.
+* Return: Always 0.
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	int num1, num2;
+	char *op;
 
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-/**
-* op_add - Returns the sum of two numbers.
-* @a: The first number.
-* @b: The second number.
-* Return: The sum of a and b.
-*/
-int op_add(int a, int b)
-{
-	return (a + b);
-}
-/**
-* op_sub - Returns the difference of two numbers.
-* @a: The first number.
-* @b: The second number.
-* Return: The difference of a and b.
-*/
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-/**
-* op_mul - Returns the product of two numbers.
-* @a: The first number.
-* @b: The second number.
-* Return: The product of a and b.
-*/
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-/**
-* op_div - Returns the division of two numbers.
-* @a: The first number.
-* @b: The second number.
-* Return: The quotient of a and b.
-*/
-int op_div(int a, int b)
-{
-	return (a / b);
-}
-/**
-* op_mod - Returns the remainder of the division of two numbers.
-* @a: The first number.
-* @b: The second number.
-* Return: The remainder of the division of a by b.
-*/
-int op_mod(int a, int b)
-{
-	return (a % b);
+
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+
+
+	return (0);
 }
