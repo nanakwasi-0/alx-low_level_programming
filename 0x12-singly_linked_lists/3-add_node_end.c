8183c21a9f45f872489a00ce4f341698fa923250
Nana <nankwasi999@gmail.com>
2023-07-27 21:25:34 -0700

    Singly Linked Lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,7 +13,6 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
 	list_t *temp = *head;
-	
 	unsigned int len = 0;
 	
 	while (str[len])
@@ -30,9 +29,11 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = new;
 		return (new);
 	}
+	
 	while (temp->next)
 		temp = temp->next;
-		temp->next = new;
+	
+	temp->next = new;
 		
 	return (new);
 }
