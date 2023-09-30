d65921573581045e060e11fab521638c127f865f
nanakwasi-0 <nankwasi999@gmail.com>
2023-09-29 17:16:15 -0700

    Whitespace

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
  * @b: string containing the binary number.
@@ -19,6 +18,5 @@ unsigned int binary_to_uint(const char *b)
                         return (0);
                 dec_val = 2 * dec_val + (b[i] - '0');
         }
-
-        return (dec_val);
+	return (dec_val);
 }
