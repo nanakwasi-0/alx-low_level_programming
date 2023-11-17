0a4046e50babb76ba4469c3c19548792308c20f4
nanakwasi-0 <nankwasi999@gmail.com>
2023-11-16 23:48:34 -0800

    Task 1

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - a function to check for uppercase characters.
+ * @c: this is the formal parameter.
+ * Return: 1 on success and 0 on failure
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - This function computes the absolute value of an int
+ * @n: The value to be converted.
+ * Return: 0(Success!!)
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
